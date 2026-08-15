b9be7b898f964b30a770fae21b02ae2a62c96086
muntasirasif <muntasirashifee@gmail.com>
2026-08-15 12:33:49 +0600

    feat: update login screen layout and add application icon asset

--- lib/src/feature/authentication/presentation/view/login/login_screen.dart
+++ lib/src/feature/authentication/presentation/view/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../../core/gen/assets.gen.dart';
 import '../../../../../../core/static/theme/theme.dart';
 import 'login_form.dart';
 
@@ -11,69 +12,58 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 600.w),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: context.padding.p16.r),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: context.padding.p16.r),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                50.verticalSpace,
+                Assets.images.appIcon.image(height: 200.h),
+                10.verticalSpace,
+                Text(
+                  'Sign In',
+                  style: context.textStyle.headlineLarge.copyWith(
+                    color: context.color.primary,
+                  ),
+                ),
+                8.verticalSpace,
+                Text(
+                  'Log in to your account',
+                  style: context.textStyle.bodyMedium,
+                ),
+                20.verticalSpace,
+
+                // Form
+                const SignInForm(),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    50.verticalSpace,
-
-                    10.verticalSpace,
-                    Center(
+                    Text(
+                      "Don't have an account?",
+                      style: context.textStyle.bodyMedium,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
                       child: Text(
-                        'Sign In',
-                        style: context.textStyle.headlineLarge.copyWith(
+                        'Create account',
+                        style: context.textStyle.bodyMedium.copyWith(
                           color: context.color.primary,
+                          decoration: TextDecoration.underline,
+                          decorationColor: context.color.primary,
                         ),
                       ),
                     ),
-                    8.verticalSpace,
-                    Center(
-                      child: Text(
-                        'Log in to your account',
-                        style: context.textStyle.bodyMedium,
-                      ),
-                    ),
-                    20.verticalSpace,
-
-                    // Form
-                    const SignInForm(),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account?",
-                          style: context.textStyle.bodyMedium,
-                        ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Text(
-                            'Create account',
-                            style: context.textStyle.bodyMedium.copyWith(
-                              color: context.color.primary,
-                              decoration: TextDecoration.underline,
-                              decorationColor: context.color.primary,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    20.verticalSpace,
-                    Center(
-                      child: Text('or', style: context.textStyle.bodyMedium),
-                    ),
-                    20.verticalSpace,
-
-                    // google sign in button
                   ],
                 ),
-              ),
+                20.verticalSpace,
+                Center(child: Text('or', style: context.textStyle.bodyMedium)),
+                20.verticalSpace,
+
+                // google sign in button
+              ],
             ),
           ),
         ),
